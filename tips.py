8884c00012e6a4d0023507db25220ed2d868d0ef
Zhupenglong <1192490075@qq.com>
2022-10-15 15:12:43 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -3,7 +3,7 @@ from win11toast import toast
 
 def tip():
     icon = {
-        'src': r'C:\Users\11924\PycharmProjects\RequestsConnect\深圳技术大学校徽.ico',
+        'src': r'深圳技术大学校徽.ico',
         'placement': 'appLogoOverride'
     }
     toast('SZTU自动联网', '连接成功', icon=icon, duration='short')
